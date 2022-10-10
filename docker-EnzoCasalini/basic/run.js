0f89e401f40bfed48b3e89281ff11f6f9a202e41
EnzoCasalini <enzocasa83@gmail.com>
2022-10-10 16:29:49 +0200

    wouhhhou

--- docker-EnzoCasalini/basic/run.js
+++ docker-EnzoCasalini/basic/run.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World (nodeJS) !');
+  res.send('Hello World (nodeJS) woow !');
 });
 
 app.listen(PORT, HOST, () => {
